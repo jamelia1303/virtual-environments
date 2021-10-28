57a74628f2a372dbc538747e74a399a03a8c4d8e
Tatiana Terekhina <Tatiana.Terekhina@mii.com>
2021-10-28 13:34:55 -0400

    Fixed

--- images/win/scripts/Installers/Configure-Antivirus.ps1
+++ images/win/scripts/Installers/Configure-Antivirus.ps1
@@ -3,3 +3,8 @@ Set-MpPreference -ScanAvgCPULoadFactor 5 -ExclusionPath "D:\", "C:\"
 
 Write-Host "Disable Antivirus"
 Set-MpPreference -DisableRealtimeMonitoring $true
+
+$atpRegPath = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows Advanced Threat Protection'
+if (Test-Path $atpRegPath) {
+    Set-ItemProperty -Path $atpRegPath -Name 'ForceDefenderPassiveMode' -Value '1' -Type 'DWORD'
+}
